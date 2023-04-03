8faeddd5d11d0c701919a0b4000c1289371c0ea8
Tania1912Senai <125280697+Tania1912Senai@users.noreply.github.com>
2023-04-03 18:51:30 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+//Atividade em sala de aula 03.04.2023
 //Calculadora
-console.WriteLine()
+
+//Entrada
+console.WriteLine("Programando uma Calculadora")
 console.WriteLine(@$"informe a operacao matematica desejada");
-Informe a operacao desejada
+
 '+' para adicao
 '*' para multiplicacao
 '-' para subtracao
 '/' para divisao
+
+char operacaomatematica = char.Parse(console.ReadLine());
+
+//Temos que ter uma variavel numero para chegar ao resultado da operaçao
+
+float resultado = O
+
+console.Writeline($"digite o primeiro numero:");
+
+float. numero1 = float.Parse(Console.RedLine());
+
+console.WriteLine($"digite o segundo numero:");
+
+float.numero2 = float.Parse(Console.RedLine();
+                            
+//Processamento
+  
+switch(value1)
+case'+" 
+{(resultado = (numero1 + numero2);
+console.WriteLine($"o resultado da soma é {resultado}");}
+break;
+case'-'
+{(resultado = (numero1 - numero2);
+console.WriteLine($"o resultado da subtracao é {resultado}");}
+break;
+case'*'
+{(resultado = (numero1 * numero2);
+console.WriteLine($"o resultado da multiplicacao é {resultado}");}
+break;
+case'/'
+{(resultado = (numero1 / numero2);
+console.WriteLine($"o resultado da divisão é {resultado}");}
+break;   
+                            
+default:
+console.WriteLine($"A operacaomatematica informada nao e suportada pela nossa calculadora");
+break;                           
+
