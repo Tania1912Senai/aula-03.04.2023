5e8365469c761a1edc0375e83d6629b95adc606a
Tania De Oliveira Lima Dos Santos <1185849874@info.local>
2023-04-03 11:00:28 -0300

    aula 03.04.2023

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Calculadora
+console.WriteLine()
+console.WriteLine(@$"informe a operacao matematica desejada");
+Informe a operacao desejada
+'+' para adicao
+'*' para multiplicacao
+'-' para subtracao
+'/' para divisao
