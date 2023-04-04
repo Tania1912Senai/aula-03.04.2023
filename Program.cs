e0b3d8c6812f9a2abb9de863f2c29ad92773aec6
Tania De Oliveira Lima Dos Santos <1185849874@info.local>
2023-04-04 08:34:44 -0300

    03.04.2023 corrigido

--- Program.cs
+++ Program.cs
@@ -4,49 +4,49 @@
 //Calculadora
 
 //Entrada
-console.WriteLine("Programando uma Calculadora")
-console.WriteLine(@$"informe a operacao matematica desejada");
+Console.WriteLine("Programando uma Calculadora");
+Console.WriteLine(@$"informe a operacao matematica desejada
+'+' para adicao,
+'*' para multiplicacao,
+'-' para subtracao,
+'/' para divisao");
 
-'+' para adicao
-'*' para multiplicacao
-'-' para subtracao
-'/' para divisao
-
-char operacaomatematica = char.Parse(console.ReadLine());
+char operacaomatematica = char.Parse(Console.ReadLine());
 
 //Temos que ter uma variavel numero para chegar ao resultado da operaçao
 
-float resultado = O
+float resultado = 0;
 
-console.Writeline($"digite o primeiro numero:");
+Console.WriteLine($"digite o primeiro numero:");
 
-float. numero1 = float.Parse(Console.RedLine());
+float numero1 = float.Parse(Console.ReadLine());
 
-console.WriteLine($"digite o segundo numero:");
+Console.WriteLine($"digite o segundo numero:");
 
-float.numero2 = float.Parse(Console.RedLine();
-                            
+float numero2 = float.Parse(Console.ReadLine());
+
 //Processamento
-  
-switch(value1)
-case'+" 
-{(resultado = (numero1 + numero2);
-console.WriteLine($"o resultado da soma é {resultado}");}
-break;
-case'-'
-{(resultado = (numero1 - numero2);
-console.WriteLine($"o resultado da subtracao é {resultado}");}
-break;
-case'*'
-{(resultado = (numero1 * numero2);
-console.WriteLine($"o resultado da multiplicacao é {resultado}");}
-break;
-case'/'
-{(resultado = (numero1 / numero2);
-console.WriteLine($"o resultado da divisão é {resultado}");}
-break;   
-                            
-default:
-console.WriteLine($"a operacaomatematica informada nao e suportada pela nossa calculadora");
-break;                           
 
+switch (operacaomatematica)
+
+{
+    case '+':
+        resultado = (numero1 + numero2);
+        Console.WriteLine($"o resultado da soma é {resultado}");
+        break;
+    case '-':
+        resultado = (numero1 - numero2);
+        Console.WriteLine($"o resultado da subtracao é {resultado}");
+        break;
+    case '*':
+        resultado = (numero1 * numero2);
+        Console.WriteLine($"o resultado da multiplicacao é {resultado}");
+        break;
+    case '/':
+        resultado = (numero1 / numero2);
+        Console.WriteLine($"o resultado da divisão é {resultado}");
+        break;
+    default:
+        Console.WriteLine($"a operacaomatematica informada nao e suportada pela nossa calculadora");
+        break;
+}
